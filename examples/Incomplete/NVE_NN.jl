d1522a4885438c84288cf3c2a8812a1bb4ed1331
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-05-09 18:02:33 +0530

    updates in example files

--- examples/Incomplete/NVE_NN.jl
+++ examples/Incomplete/NVE_NN.jl
@@ -2,13 +2,14 @@
 using DiffEqFlux
 using IterTools: ncycle
 using MDSimulator
-using ParticlesMesh
 using Plots
 using Flux
 using LinearAlgebra
 using ForwardDiff
 using Unitful
 using DiffEqSensitivity
+using PDMesh
+
 
 # Setup
 T = 100.0u"K"
@@ -16,7 +17,7 @@ m = 39.95u"g/mol"
 L = uconvert(UNITS.distance, 15u"Å")
 Δτ = 1.0u"fs"
 
-u0 = L*rectangular([1/2, 1, 1],[2, 1, 1])
+u0 = L/2 *create(Cuboid([0 2;0 2;0 2]), resolution=1.0, rand_=0.05)[1]
 v0 = 0u0/1u"fs"
 
 N = size(u0, 2)
@@ -32,17 +33,16 @@ R = min(2.5σpot, sqrt(3)/2*L)
 interatomic_potentials = [LennardJonesParameters(ϵ, σpot, R)]
 
 # Define simulation object
-sim = SimInfo(u0, v0, mass, interatomic_potentials, boundary_condition, true; Δτ = Δτ, save_every = 100, thermo_save_every = 100, max_neighs_hard_set=150, reneighboring_every=1_000_000,)
+sim = MDSimulator.SimInfo(u0, v0, mass, interatomic_potentials, boundary_condition, true; Δτ = Δτ, save_every = 100, thermo_save_every = 100, max_neighs_hard_set=150, reneighboring_every=1_000_000,)
 
 # Define ensemble
 ensemble = [NVE()]
 
 # Callbacks
-cbs = [cb_reneighboring_f, cb_savethermo_f]
-# cbs = []
+callbacks = [cb_reneighboring_f, cb_savethermo_f]
 
 # Simulate for n number of steps
-sol, parameters = simulate(10000, sim, ensemble, cbs=cbs, verbose=true)
+sol, parameters = MDSimulator.simulate(10000, sim, ensemble, callbacks=callbacks, verbose=true)
    
 ẍdata = 1parameters.S.acc
 xdata = [1i.x[2] for i in sol.u]
@@ -50,25 +50,26 @@ xdata = [1i.x[2] for i in sol.u]
 
 
 model = Chain((x)->[x], Dense(1, 3, σ), Dense(3, 1, σ), (x)->first(x))
-pot1 =  NNPair(model)
+pot1 =  NNPair([model])
 
+# define function to get cutoff
 MDSimulator.cutoff(pot::NNPair) = R
 
-θ, re = Flux.destructure(pot1.model)
+θ, re = Flux.destructure(pot1.energy[1,1])
 
-sim2 = SimInfo(u0, v0, mass, [pot1], boundary_condition, false; Δτ = Δτ, save_every = 100, thermo_save_every = 100, max_neighs_hard_set=150, reneighboring_every=1_000_000,)
+sim2 = MDSimulator.SimInfo(u0, v0, mass, [pot1], boundary_condition, false; Δτ = Δτ, save_every = 100, thermo_save_every = 100, max_neighs_hard_set=150, reneighboring_every=1_000_000,)
 # parameters2 = MDSimulator.exe_at_start(sim2, 10000, false)
 
-sim2.interatomic_potentials[1] = pot1
-sol2, parameters2 = simulate(10000, sim2, ensemble, cbs=cbs, verbose=false)
+sim2.interatomic_potentials[1][1] = [pot1]
+sol2, parameters2 = MDSimulator.simulate(10000, sim2, ensemble, callbacks=callbacks, verbose=false)
 
 function loss(θ)
     model = re(θ)
-    sim2.interatomic_potentials[1].model.layers[2].W .= model.layers[2].W
-    sim2.interatomic_potentials[1].model.layers[2].b .= model.layers[2].b
-    sim2.interatomic_potentials[1].model.layers[3].W .= model.layers[3].W
-    sim2.interatomic_potentials[1].model.layers[3].b .= model.layers[3].b
-    sol2, parameters2 = simulate(10000, sim2, ensemble, cbs=cbs, verbose=false)
+    sim2.interatomic_potentials[1][1].energy[1,1].layers[2].W .= model.layers[2].W
+    sim2.interatomic_potentials[1][1].energy[1,1].layers[2].b .= model.layers[2].b
+    sim2.interatomic_potentials[1][1].energy[1,1].layers[3].W .= model.layers[3].W
+    sim2.interatomic_potentials[1][1].energy[1,1].layers[3].b .= model.layers[3].b
+    sol2, parameters2 = MDSimulator.simulate(10000, sim2, ensemble, callbacks=callbacks, verbose=false)
     l = 0.0
     for i in length(sol2.u)
         l += sum((sol.u[i].x[2] .- sol2.u[i].x[2]).^2)
@@ -78,22 +79,19 @@ end
 
 ϵ₁ = 0.1
 η = 0.0001
-for i in 1:1
-    # @show θ
+for i in 1:100
     new_θ = ones(length(θ))*θ' + I(length(θ))*ϵ₁
     l = loss(θ)
     gs = [min(max((loss(new_θ[i,:]) - l)/ϵ₁*η, -0.01*θ[i]), 0.01*θ[i]) for i in 1:length(θ)]
     θ .-= gs
-    # @show θ
-    # @show gs
     @show i l
 end
 
 
 r = 0.5:100:3000
 
-[MDSimulator.potential_force(i, sim.interatomic_potentials[1]) for i in r]
-[MDSimulator.potential_force(i, sim2.interatomic_potentials[1]) for i in r]
+[MDSimulator.potential_force(i, sim.interatomic_potentials[1][1]) for i in r]
+[MDSimulator.potential_force(i, sim2.interatomic_potentials[1][1]) for i in r]
 
 
 #
--- examples/Incomplete/simple_NN.jl
+++ examples/Incomplete/simple_NN.jl
@@ -1,33 +1,73 @@
-using Plots
-using Flux
-using IterTools: ncycle
-using Statistics
-using MDSimulator
+using Pkg; Pkg.activate("."); Pkg.instantiate();
 
+# macro import_these(pkgs...)
+#     Pkg.add.(string.(pkgs))
+#     text = foldl(*, ["using $p; " for p in pkgs])
+#     :($text)
+# end
+# Pkg.develop(path="../MakeReports")
+
+using Plots, Flux, IterTools, Statistics
+using MakeReports
+using MDSimulator
+report = Report()
+
+
+text="""
+Creating an LJ potential with following paramters:
+sigma = 3.4
+expilon = 0.99
+cutoff = 3*sigma
+"""
+push!(report, StringPart(text))
+
+# create LJ Potential
 σ_ = 3.4
 lj_params = LennardJonesParameters(0.99, σ_, 3*σ_)
 r = 1.0σ_:0.01σ_:2σ_
-𝐄 = [potential_energy(i, lj_params) + 0.05randn() for i in r]
-scatter(r, 𝐄)
-# 𝐄 = [f(i) for i in r]
+𝐄 = [potential_energy(i, lj_params, (1,1)) + 0.05randn() for i in r]
 
+# plot
+fig = scatter(r, 𝐄)
+savefig(fig, "./output/lj.png")
+push!(report, ImagePart("./output/lj.png"; desc="LJ potential."))
+
+text="""
+Create data set by normalized energy values.
+"""
+push!(report, StringPart(text))
+# creating normalized dataset
 m_ = mean(𝐄)
 std_ = std(𝐄)
 𝐄 = @. (𝐄 - m_)/std_
-
 new_lj_pot = (x) -> model(x)*std_ +m_
-
 scatter(r, 𝐄)
 
+# creating an MLP model
 k = 10
 act = σ
 model = Chain(x-> [x], Dense(1, k, act), Dense(k, k, act), Dense(k, 1), (x) -> first(x))
 ps = Flux.params(model)
 ys = model.(r)
+text="""
+Create an MLP model with following paramters:
 
-scatter(r, 𝐄)
+activation: sigmoid
+
+hidden layers: [$k, $k, $k]
+
+model: $(model)
+"""
+push!(report, StringPart(text))
+
+# plot model before training
+fig = scatter(r, 𝐄)
 plot!(r, ys)
+savefig(fig, "./output/before_training.png")
+push!(report, ImagePart("./output/before_training.png"; desc="Model before training."))
 
+
+# set loss function
 rx = 2.5σ_:0.1σ_:6σ_
 rdata = [(0.0-m_)/std_ for i in rx]
 
@@ -39,23 +79,48 @@ loss_f(x, y) = sum(@. (model(x)-y)^2) + 0.01*sum(@. ((model(rx2)-rdata2))^2 ) + 
 
 loss_full() = sum([loss_f(x, y) for (x, y) in zip(r, 𝐄)])
 
+# optmizer
 opt = ADAM(0.01, (0.9, 0.8))
 opt = ADAGrad()
+text="""
+Using ADAGrad optimizer.
+"""
+push!(report, StringPart(text))
 
-Flux.train!(loss_f, ps, ncycle([(r, 𝐄)], 100000), opt, cb = Flux.throttle(()->(@show loss_full()), 3))
+# training loop
+for i in 1:100
+    Flux.train!(loss_f, ps, ncycle([(r, 𝐄)], 1000), opt, cb = Flux.throttle(()->(@show loss_full()), 3))
+    fig = scatter(r, 𝐄)
+    plot!(r, model.(r))
+    savefig(fig, "./output/intermediate.png")
+end
+text="""
+Training for 100000 epochs.
+"""
+push!(report, StringPart(text))
 
-scatter(r, 𝐄)
-plot!(r, model.(r))
 
-
-r2 = 0.75σ_:0.01σ_:10σ_
-plot(r2, new_lj_pot.(r2))
+# Final plot
+r2 = 0.25σ_:0.01σ_:10σ_
 # scatter!(r2, [potential_energy(i, lj_params) for i in r2])
-scatter!(r2, [potential_energy(i, lj_params) for i in r2], color="red")
+fig = scatter(r2, [potential_energy(i, lj_params, (1,1)) for i in r2], label="LJ")
 # scatter!(rx2, rdata2*std_.+m_, color="green")
-scatter!(rx, rdata*std_.+m_, color="yellow")
+scatter!(rx, rdata*std_.+m_, color="yellow", label="Zero data")
 vline!([0.75σ_], lw=2)
-#
+plot!(r2, new_lj_pot.(r2); linewidth=2, label="MLP model")
+ylims!(-2, 2)
+savefig(fig, "./output/after_training1.png")
+ylims!(-2, 100)
+savefig(fig, "./output/after_training2.png")
 
+push!(report, ImagePart("./output/after_training1.png"; desc="Model after training (near mimima)."))
+push!(report, ImagePart("./output/after_training2.png"; desc="Model after training (repulsive region)."))
+
+
+# save model
 using BSON: @save
 @save "./output/trained_NN.bson" model
+push!(report, PathPart("./output/trained_NN.bson"; desc="Model after training."))
+
+#print report
+print_report("./output/simple_NN", report)
